bd7fae662e19a8b38ecf52847112f3a600bed82d
Anirudh <anirudh2403@gmail.com>
2020-08-21 00:12:31 +0530

    Adds v2 for add_one_to_number

--- arrays/add_one_to_number.rb
+++ arrays/add_one_to_number.rb
@@ -17,6 +17,31 @@ def plusOne(a)
   digits(number+1).flatten
 end
 
+
+# v2 of the soln
+
+def plusOne_v2(a)
+  carry = 1
+  len = a.length
+
+  a.each_index do |i|
+    num = a[len - 1 - i] + carry
+    a[len-1-i] = num%10
+    carry = num/10
+  end
+
+  if carry > 0
+    a.unshift(carry)
+  end
+
+  i = 0
+  while a[i] == 0
+    i+=1
+  end
+
+  return a.drop(i)
+end
+
 input = [0]
 
 pp plusOne(input)
