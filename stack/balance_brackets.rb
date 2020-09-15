f0a4812b9cf2e74aaf94951237f2ed8115cc13c4
Anirudh <anirudh2403@gmail.com>
2020-09-15 22:01:15 +0530

    Adds code for balanced Brackets

---
+++ stack/balance_brackets.rb
@@ -0,0 +1,37 @@
+
+def balance_brackets(string)
+  return true if string.empty?
+  return false if string.length.odd?
+
+  stack = []
+
+  string.each_char do |char|
+    case char
+    when '('
+      stack << char
+    when ')'
+      last_bracket = stack.pop
+      return false if last_bracket.nil?
+      return false if last_bracket != '('
+    when '{'
+      stack << char
+    when '}'
+      last_bracket = stack.pop
+      return false if last_bracket.nil?
+      return false if last_bracket != '{'
+    when '['
+      stack << char
+    when ']'
+      last_bracket = stack.pop
+      return false if last_bracket.nil?
+      return false if last_bracket != '['
+    end
+  end
+
+  return true
+end
+
+
+inputs = ['{[()]}', '{}()', '{(})', ')', '']
+
+inputs.each { |i| puts balance_brackets(i) }
